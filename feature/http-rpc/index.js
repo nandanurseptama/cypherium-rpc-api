348f78c18dbdac2554802d492a739c15e56423b9
nandanurseptama <nandanurseptama@gmail.com>
2021-11-27 07:58:37 +0000

    update

--- feature/http-rpc/index.js
+++ feature/http-rpc/index.js
@@ -2,13 +2,17 @@ const { default: axios } = require("axios")
 require('dotenv').config()
 module.exports = function(req, res, next){
     console.log(process.env.RPC_API)
-    console.log(req.body);
+    console.log(`Body : ${req.body}`);
     axios.post(
-        `${process.env.RPC_API}`,
-        req.body,
+        `${process.env.RPC_API}`,        req.body,
+
+	{
+		headers: {'Content-Type': 'application/json'},
+	},
     ).then((any)=>{
         return res.status(any.status).json(any.data);
     }).catch((onerror)=>{
+	console.log(onerror);
         return res.status(500).json({
             'message' : onerror,
             'data' : null
--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const port = 3000
 app.use(express.json())
 app.use(appAuth);
 app.post('/rpc',httpRpc);
-
+app.get('/', (req, res)=>{
+  res.status(200).json({status:'ok'});
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
