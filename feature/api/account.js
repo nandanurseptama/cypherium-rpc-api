8b28b22233909e1dd66b90b3ba77a89b39581951
nandanurseptama <nandanurseptama@gmail.com>
2022-01-09 07:55:05 -0500

    update

--- feature/api/account.js
+++ feature/api/account.js
@@ -1,9 +1,24 @@
 const { default: axios } = require("axios");
 const { getCypheriumPrice } = require("./externals");
 const BigNumber = require("bignumber.js");
-var Web3c = require('web3c');
-var web3c = new Web3c();
+var Web3c = require('@cypherium/web3c');
+var tokenAbi = require('../../constant/abis/tokenAbi.json');
 const bulkTokenBalance = async (req, res) => {
+  var apiUrl = req.query.network === 'mainnet' ? process.env.RPC_API : process.env.RPC_API_TESTNET;
+  var web3c = new Web3c(
+    new Web3c.providers.HttpProvider(
+      apiUrl,
+      null,
+      null,
+      null,
+      [
+        {
+          name: "Authorization",
+          value: "kla7771ksdasbc===",
+        },
+      ]
+    )
+  );
   console.log(req.body);
   var currency = req.body.currency;
   var cypheriumPrice = await getCypheriumPrice(currency.toLowerCase());
@@ -26,7 +41,7 @@ const bulkTokenBalance = async (req, res) => {
       var params = rpcBalanceParams;
       params.params = [body[i].address, "latest"];
       console.log(params);
-      var resAxios = await axios.post(`${process.env.RPC_API}`, params, {
+      var resAxios = await axios.post(apiUrl, params, {
         headers: { "Content-Type": "application/json" },
       });
       console.log(resAxios.data);
@@ -37,7 +52,7 @@ const bulkTokenBalance = async (req, res) => {
         });
       }
       var ui_balance = BigNumber(resAxios.data.result)
-        .dividedBy(BigNumber(Math.pow(10, 18)))
+        .dividedBy(BigNumber(Math.pow(10, body[i].decimal)))
         .toFixed();
       console.log(cypheriumPrice);
       response.push({
@@ -46,12 +61,34 @@ const bulkTokenBalance = async (req, res) => {
         money_balance: parseFloat(ui_balance) * cypheriumPrice,
         address : body[i].address,
         contractAddress : body[i].contractAddress,
+        name : "Cypherium",
+        decimal : body[i].decimal,
+        symbol : "CPH",
+      });
+    } else{
+      var tokenContract = web3c.cph.contract(tokenAbi).at(body[i].contractAddress);
+      var balanceOf = await tokenContract.balanceOf([
+        body[i].address
+      ]);
+      var symbol = await tokenContract.symbol();
+      var name = await tokenContract.name();
+      var decimal = await tokenContract.decimals();
+      var ui_balance = BigNumber(balanceOf)
+        .dividedBy(BigNumber(Math.pow(10, decimal)))
+        .toFixed();
+      response.push({
+        balance: BigNumber.BigNumber(balanceOf).toFixed(),
+        ui_balance: ui_balance,
+        money_balance: parseFloat(ui_balance) * 0,
+        address : body[i].address,
+        contractAddress : body[i].contractAddress,
+        name : name,
+        decimal : decimal,
+        symbol : symbol,
       });
     }
-  } else{
-
   }
-  return res.  status(200).json(response);
+  return res.status(200).json(response);
 };
 module.exports = accountsEndpoint = {
   bulkTokenBalance,
